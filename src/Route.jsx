d3ea5aafd0015cbc26438e7237608bebbab9b6b8
AbdullahShwareb <shwareba700@gmail.com>
2026-01-29 22:12:36 +0200

    fix conta

--- src/Route.jsx
+++ src/Route.jsx
@@ -11,9 +11,9 @@ import Register from "./pages/register/Register.jsx";
 
 import Shop from "./pages/shop/Shop.jsx";
 import About from "./pages/about/About.jsx";
-import Services from "./pages/services/services.jsx";
+import Services from "./pages/services/Services.jsx";
 import Blog from "./pages/blog/Blog.jsx";
-import Contact from "./pages/contact/Contact.jsx";
+import Contact from "./pages/contact/contact.jsx";
 
 import Categories from "./components/Categories/Categories.jsx";
 
