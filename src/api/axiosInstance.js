668cadcc449a98aece91ab2b3c888715b20c47ab
AbdullahShwareb <shwareba700@gmail.com>
2026-01-30 15:55:35 +0200

    ren

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
+const isProd = import.meta.env.PROD; 
+
 const axiosInstance = axios.create({
-    baseURL: "/api",
+  baseURL: isProd
+    ? "https://knowledgeshop.runasp.net/api" 
+    : "/api",                               
 });
 
 axiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = "Bearer " + token;
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = "Bearer " + token;
 
-    config.headers["Accept-Language"] = "en";
-    return config;
+  config.headers["Accept-Language"] = "en";
+  return config;
 });
 
 export default axiosInstance;
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "https://knowledgeshop.runasp.net",
-                changeOrigin: true,
-                secure: false,
-            },
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://knowledgeshop.runasp.net",
+        changeOrigin: true,
+        secure: false,
+      },
     },
+  },
 });
